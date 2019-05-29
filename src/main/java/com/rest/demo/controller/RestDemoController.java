aee5515ed62befe60ec99121147342aecb002149
Harikrishnabhuvaneshwari <harikrishnabhuvaneshwari@yahoo.com>
2019-05-29 21:36:25 +0530

    #played

--- src/main/java/com/rest/demo/controller/RestDemoController.java
+++ src/main/java/com/rest/demo/controller/RestDemoController.java
@@ -1,5 +1,6 @@
 package com.rest.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rest.demo.entity.Employee;
 
 @RestController
-@RequestMapping("rest/")
+@RequestMapping("/rest")
 public class RestDemoController {
 
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "Hello World";
+	}
+	
 	@PostMapping(value="post/",consumes={"application/json"})
 	private Employee postRequest(@RequestBody Employee employee){
 		return employee;
 	}
+	
+	
+	
+	
 
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+<hr>
+<a href="rest/hello">Hello</a>
 </body>
 </html>
